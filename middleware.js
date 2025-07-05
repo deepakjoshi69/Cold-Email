229199c78afec3d19a680e4597d3ab9b99acc056
HimanshuXDebug <hjjoshi020@gmail.com>
2025-07-06 02:36:50 +0530

    5th attempt middleware changes for deployment

--- middleware.js
+++ middleware.js
@@ -1,17 +1,18 @@
-import { auth, clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)'
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth.protect();  // ✅ this actually protects the route
+  }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-
     '/(api|trpc)(.*)',
   ],
 };
